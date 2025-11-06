b0b54bb28fa7c3075cd5aeed36e0567796edc56b
hhhannahmmmontana <rubakhin.edu@niuitmo.ru>
2025-11-06 13:20:54 +0300

    feat: animation

--- src/pages/promo/components/promo-section-1.component.js
+++ src/pages/promo/components/promo-section-1.component.js
@@ -5,17 +5,28 @@ class PromoSection1 extends HTMLElement {
         const baseUrl = import.meta.env.BASE_URL;
         this.innerHTML = /*html*/`
             <section class="promo__section-1">
-                <div class="promo__section-1__img-title">
-                    <h1 class="title-h1-italic promo__section-1__title">
-                        Make your dream come true<br />or decorate your home
-                    </h1>
-                    <img
-                        src=${ceramicVase}
-                        alt="vase"
-                        class="promo__img promo__section-1__img"
-                    />
+                <div class="container">
+                    <div class="content">
+                        <header-component></header-component>
+                    </div>
                 </div>
-                <a href="${baseUrl}catalog"><button class="btn-default promo__section-1__btn">shop now</button></a>
+                <content class="promo__section-1__content">
+                    <div class="promo__section-1__img-title">
+                        <h1 class="title-h1-italic promo__section-1__title">
+                            Make your dream come true<br />or decorate your home
+                        </h1>
+                        <img
+                            src=${ceramicVase}
+                            alt="vase"
+                            class="promo__img promo__section-1__img slided-in"
+                        />
+                    </div>
+                    <a href="${baseUrl}catalog">
+                        <button class="btn-default promo__section-1__btn slided-in">
+                            shop now
+                        </button>
+                    </a>
+                </content>
             </section>
         `
     }
